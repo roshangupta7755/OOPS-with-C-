4b82e682ccf454ca53d4550a3bcad464aebe5f38
Roshan Gupta <roshangupta112113@gmail.com>
2025-11-23 17:37:17 +0530

    Add files via upload

---
+++ Day 3/01_Inheritance.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+public:
+    string name;
+    int age;
+
+    Person() {
+        // default constructor
+    }
+};
+
+class Student : public Person {
+public:
+    int rollno;
+
+    void getInfo() {
+        cout << "name : " << name << endl;
+        cout << "age  : " << age << endl;
+        cout << "rollno : " << rollno << endl;
+    }
+};
+
+int main() {
+    Student s1;
+    s1.name = "rahul kumar";
+    s1.age = 21;
+    s1.rollno = 123;
+
+    s1.getInfo();
+    
+    return 0;
+}
